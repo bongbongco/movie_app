2ea5d69212b8410fe5baf771ef8093e3882a8eaf
seungyonglee <bongbongco@gmail.com>
2018-02-10 23:04:41 +0900

    Movie, MoviePoster funtional 컴포넌트로 수정

--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -2,31 +2,25 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-class Movie extends Component {
-  static propTypes = {
-    title: PropTypes.string,
-    poster: PropTypes.string
-  };
 
-  render() {
-    console.log(this.props);
-    return (
-      <div>
-        <MoviePoster poster={this.props.poster} />
-        <h1>{this.props.title}</h1>
-      </div>
-    );
-  }
+function Movie ({title, poster}){
+  return (
+    <div>
+    <MoviePoster poster={poster} />
+    <h1>{title}</h1>
+    </div>
+  );
 }
 
-class MoviePoster extends Component {
-  static propTypes = {
-    poster: PropTypes.string.isRequired
-  }
+function MoviePoster({poster}){
+  return (
+    <img src={poster} alt="Movie Poster" />
+  );
+}
 
-  render() {
-    return <img src={this.props.poster} />;
-  }
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequire
 }
 
 export default Movie;
