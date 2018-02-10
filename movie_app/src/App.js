5345de80e808e9f9535b9b9ae3de5343ff8d514c
seungyonglee <bongbongco@gmail.com>
2018-02-10 11:42:51 +0900

    setState() 테스트

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -2,28 +2,6 @@ import React, { Component } from "react";
 import "./App.css";
 import Movie from "./Movie";
 
-const movies = [
-  {
-    title: "Matrix",
-    poster:
-      "https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-  },
-  {
-    title: "Full Metal Jacket",
-    poster:
-      "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg"
-  },
-  {
-    title: "Old boy",
-    poster:
-      "https://upload.wikimedia.org/wikipedia/en/b/bb/Oldboy_2013_film_poster.jpg"
-  },
-  {
-    title: "Star Wars",
-    poster:
-      "https://lumiere-a.akamaihd.net/v1/images/the-last-jedi-theatrical-poster-film-page_bca06283.jpeg?region=0%2C0%2C480%2C711"
-  }
-];
 
 class App extends Component {
 
@@ -31,7 +9,29 @@ class App extends Component {
 // Update: componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillupdate() -> render() -> componentDidUpdate()
 
   state = {
-    greeting: "Hello!"
+    greeting: "Hello!",
+    movies : [
+      {
+        title: "Matrix",
+        poster:
+          "https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
+      },
+      {
+        title: "Full Metal Jacket",
+        poster:
+          "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg"
+      },
+      {
+        title: "Old boy",
+        poster:
+          "https://upload.wikimedia.org/wikipedia/en/b/bb/Oldboy_2013_film_poster.jpg"
+      },
+      {
+        title: "Star Wars",
+        poster:
+          "https://lumiere-a.akamaihd.net/v1/images/the-last-jedi-theatrical-poster-film-page_bca06283.jpeg?region=0%2C0%2C480%2C711"
+      }
+    ]
   }
 
   componentWillMount() {
@@ -42,9 +42,15 @@ class App extends Component {
     console.log("did mount")
     setTimeout(() => {
       this.setState({
-        greeting: "Hello again!"
+        movies: [
+          ...this.state.movies,
+          {
+            title: "Transpotting",
+            poster: "https://upload.wikimedia.org/wikipedia/en/7/71/Trainspotting_ver2.jpg"
+          }
+        ]
       })
-    }, 5000)
+    }, 2000)
   }
 
   render() {
@@ -52,7 +58,7 @@ class App extends Component {
     return (
       <div className="App">
       {this.state.greeting}
-        {movies.map((movie, index) => {
+        {this.state.movies.map((movie, index) => {
           return (
             <Movie title={movie.title} poster={movie.poster} key={index} />
           );
